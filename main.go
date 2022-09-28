ace665dd1912cb33a67accb825427a4657ee402d
ccremer <github.account@chrigel.net>
2022-09-28 13:38:27 +0200

    cli: remove compilation date
    It's unused

--- main.go
+++ main.go
@@ -21,8 +21,6 @@ var (
 
 	appName     = "provider-cloudscale"
 	appLongName = "Crossplane provider that deploys resources on cloudscale.ch"
-
-	envPrefix = ""
 )
 
 func init() {
@@ -45,10 +43,9 @@ func newApp() (context.Context, context.CancelFunc, *cli.App) {
 	logInstance := &atomic.Value{}
 	logInstance.Store(logr.Discard())
 	app := &cli.App{
-		Name:     appName,
-		Usage:    appLongName,
-		Version:  fmt.Sprintf("%s, revision=%s, date=%s", version, commit, date),
-		Compiled: compilationDate(),
+		Name:    appName,
+		Usage:   appLongName,
+		Version: fmt.Sprintf("%s, revision=%s, date=%s", version, commit, date),
 
 		EnableBashCompletion: true,
 
@@ -94,25 +91,10 @@ func rootAction(hasSubcommands bool) func(context *cli.Context) error {
 	}
 }
 
-// env combines envPrefix with given suffix delimited by underscore.
-func env(suffix string) string {
-	return envPrefix + "_" + suffix
-}
-
-// envVars combines envPrefix with each given suffix delimited by underscore.
 func envVars(suffixes ...string) []string {
 	arr := make([]string, len(suffixes))
 	for i := range suffixes {
-		arr[i] = env(suffixes[i])
+		arr[i] = suffixes[i]
 	}
 	return arr
-}
-
-func compilationDate() time.Time {
-	compiled, err := time.Parse(time.RFC3339, date)
-	if err != nil {
-		// an empty Time{} causes cli.App to guess it from binary's file timestamp.
-		return time.Time{}
-	}
-	return compiled
 }
